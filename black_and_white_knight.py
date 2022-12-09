82e44b1c35af745b48e32fb776d61b05c2efe8b6
amirkhan1092 <amirkhan1092@gmail.com>
2022-12-09 09:01:25 +0530

    problem

---
+++ black_and_white_knight.py
@@ -0,0 +1,14 @@
+'''
+#Function to find out the number of ways we can place a black and a white
+#Knight on this chessboard such that they cannot attack each other.
+'''
+def numOfWays(M, N):
+    pass
+
+
+
+# main code
+if __name__ == '__main__':
+    M = int(input())
+    N = int(input())
+    print(numOfWays(M, N))
